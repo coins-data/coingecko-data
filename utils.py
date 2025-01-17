8f470dea61649da4d6f062ea607fb2d59aeb3d61
thecybordcoder <jram93x@gmail.com>
2025-01-18 04:01:12 +0800

    truncate long data

--- utils.py
+++ utils.py
@@ -1,21 +1,29 @@
 from functools import wraps
+from pprint import pprint
+import io
 
 def test_function(func, *args, **kwargs):
     """
-        Test the function and prints its return output
+        Test the function and prints its return output (truncate if too long)
         Returns True if the function runs without errors
     """
 
     try:
-        original_func = func
         print(f"Testing {func.__name__}")
         data = func(*args, **kwargs)
-        print(f"{original_func.__name__} test passed!")
-        print(data)
+        print(f"{func.__name__} test passed!")
+        output = io.StringIO()
+        pprint(data, stream=output, depth=3, compact=True)
+        formatted_data_str = output.getvalue()
+        output.close()
+        if len(formatted_data_str) > 1000:
+            print(formatted_data_str[:1000] + '... (truncated)')
+        else:
+            print(formatted_data_str)
         print()
         return True
     except Exception as e:
-        print(f"{original_func.__name__} test failed: {e}")
+        print(f"{func.__name__} test failed: {e}")
         print()
         return False
 
