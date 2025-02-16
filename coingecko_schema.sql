5faf8d797c9f996812c134931db25425e12dffcf
thecybordcoder <jram93x@gmail.com>
2025-02-17 02:26:21 +0800

    add fields to track

--- coingecko_schema.sql
+++ coingecko_schema.sql
@@ -1,12 +1,17 @@
 CREATE TABLE coins (
     id VARCHAR(255) PRIMARY KEY,
+    symbol VARCHAR(15) NOT NULL,
     name VARCHAR(255) NOT NULL,
     ticker VARCHAR(15) NOT NULL,
     website VARCHAR(255),
-    track_hourly BOOLEAN DEFAULT FALSE,
+    image_url VARCHAR(255),
+    market_cap_rank INT,
+    market_cap_usd BIGINT,
+    fully_diluted_valuation BIGINT,
     total_supply BIGINT,
     max_supply BIGINT,
-    circulating_supply BIGINT
+    circulating_supply BIGINT,
+    update_hourly BOOLEAN DEFAULT FALSE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -15,7 +20,9 @@ CREATE TABLE continuous_btc_prices (
     timestamp TIMESTAMP NOT NULL,
     price DOUBLE PRECISION NOT NULL,
     vol_24h INT,
-    change_24h REAL,
+    high_24h DOUBLE PRECISION,
+    low_24h DOUBLE PRECISION,
+    price_change_percentage_24h REAL,
     PRIMARY KEY (coin_id, timestamp),
     FOREIGN KEY (coin_id) REFERENCES coins(id)
 );
@@ -25,7 +32,9 @@ CREATE TABLE continuous_usd_prices (
     timestamp TIMESTAMP NOT NULL,
     price DOUBLE PRECISION NOT NULL,
     vol_24h BIGINT,
-    change_24h REAL,
+    high_24h DOUBLE PRECISION,
+    low_24h DOUBLE PRECISION,
+    price_change_percentage_24h REAL,
     PRIMARY KEY (coin_id, timestamp),
     FOREIGN KEY (coin_id) REFERENCES coins(id)
 );
