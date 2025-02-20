73f04d8580628a30cf4bfc31d922fee98046280f
thecybordcoder <jram93x@gmail.com>
2025-02-21 02:51:11 +0800

    add random to avoid including same results when rank and time is equal

--- db_scripts/custom_db_functions.sql
+++ db_scripts/custom_db_functions.sql
@@ -11,9 +11,13 @@ BEGIN
     FROM coins
     WHERE (coins.update_hourly = TRUE AND coins.updated_at < CURRENT_TIMESTAMP AT TIME ZONE 'UTC' - INTERVAL '1 hour')
        OR coins.id IN (
-           SELECT coins.id
-           FROM coins
-           ORDER BY coins.market_cap_rank - (EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP AT TIME ZONE 'UTC' - coins.updated_at)) / 60)
+           SELECT 
+                coins.id
+           FROM 
+                coins
+           ORDER BY 
+                coins.market_cap_rank - (EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP AT TIME ZONE 'UTC' - coins.updated_at)) / 60),
+                random()
            LIMIT 500
        );
 END;
